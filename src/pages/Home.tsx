47dc85fdf363892bedf76edc1180b9a67329a307
64576 <64576@etu.he2b.be>
2025-04-30 13:24:59 +0200

    modified:   src/pages/Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { db } from '../firebase-config'; // Importer la configuration Firestore
 import { collection, getDocs } from 'firebase/firestore';
 import TabBar from '../components/tabbar';
